dfff9b8149fd4ff0f9e77bc4838f264b080b800d
unknown <tobiedesgreniers300@gmail.com>
2019-02-06 20:26:34 -0500

    Temp Commit (Update Temps Réel)

--- project-ui/src/app/data-display/data-display.component.ts
+++ project-ui/src/app/data-display/data-display.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import 'chart.js/src/chart';
-declare var Chart;
+//import { DataService } from '../services/data-service.service';
 
 @Component({
   selector: 'app-data-display',
@@ -8,11 +7,12 @@ declare var Chart;
   styleUrls: ['./data-display.component.css']
 })
 export class DataDisplayComponent implements OnInit {
-
+  public test = 0;
+  public timer: any;
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [0], label: 'Speed'},
+    /* {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'} */
   ];
   public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions:any = {
@@ -47,11 +47,28 @@ export class DataDisplayComponent implements OnInit {
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
 
-  
-  constructor() { }
-
-  ngOnInit() {
+  constructor(/* private dataService: DataService */) { 
 
   }
 
+  ngOnInit() {
+    console.log(this.lineChartData);
+    let _this = this;
+    this.timer = setInterval(function() {
+      _this.generateRandom()
+
+    }, 1000)
+  }
+
+  public max = 15;
+  public min = 0;
+  public compteur = 3;
+  generateRandom(): void{
+
+    console.log(this.lineChartData)
+    console.log(this.lineChartData[0].data[0])
+    this.lineChartData[0].data.push(1); //Math.floor(Math.random()*(this.max-this.min+1)+this.min);
+    this.compteur++;
+    
+  }
 }
--- project-ui/src/app/services/data-service.service.spec.ts
+++ project-ui/src/app/services/data-service.service.spec.ts
@@ -1,5 +1,4 @@
-import { TestBed } from '@angular/core/testing';
-
+/* import { TestBed } from '@angular/core/testing';
 import { DataServiceService } from './data-service.service';
 
 describe('DataServiceService', () => {
@@ -10,3 +9,4 @@ describe('DataServiceService', () => {
     expect(service).toBeTruthy();
   });
 });
+ */
