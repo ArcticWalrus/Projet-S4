7f1e56253385b7b71baf86156f957b9a06f743f1
Kitty-kat-lol <32311727+Kitty-kat-lol@users.noreply.github.com>
2019-03-19 12:02:42 -0400

    fixed magnetic and tampon, compter

--- FPGA_logic/S4_Velocyraptor/FPGA-Logic.srcs/sources_1/new/compteur_signal.vhd
+++ FPGA_logic/S4_Velocyraptor/FPGA-Logic.srcs/sources_1/new/compteur_signal.vhd
@@ -7,6 +7,7 @@ entity compteur_signal is
         line_in         : in signed(11 downto 0);
         i_clk, i_reset  : in std_logic;
         o_nb_items      : out unsigned(5 downto 0);
+        i_stb_tampon    : in std_logic;
         o_high          : out std_logic
   );
 end compteur_signal;
@@ -16,9 +17,10 @@ architecture Behavioral of compteur_signal is
 component tamponcirc is
     Port (
             i_clk, it_reset     : in std_ulogic;
+            i_decrement_stb     : in std_ulogic;
             i_WR                : in std_ulogic;
             o_plein, o_vide     : out std_ulogic;
-            o_nb_items : out unsigned( 5 downto 0 )
+            o_nb_items          : out unsigned( 5 downto 0 )
     );
 end component;
 
@@ -30,31 +32,32 @@ component detectzero is
     );
 end component;
 
-    signal s_nb_items : std_logic_vector(7 downto 0);
-    signal s_detect : std_logic;
+    signal s_nb_items       : std_logic_vector(7 downto 0);
+    signal s_detect         : std_logic;
     
-    signal s_plein, s_vide: std_ulogic;
-    signal s_s, s_z: std_ulogic;
+    signal s_plein, s_vide  : std_ulogic;
+    signal s_s, s_z         : std_ulogic;
 
 begin
 
 iteration_tampon : tamponcirc
     Port map(
-            i_clk => i_clk,
-            it_reset => i_reset,
-            i_WR => s_z,
-            o_plein => s_plein,
-            o_vide => s_vide,
-            o_nb_items => o_nb_items
+            i_clk           => i_clk,
+            it_reset        => i_reset,
+            i_decrement_stb => i_stb_tampon,
+            i_WR            => s_z,
+            o_plein         => s_plein,
+            o_vide          => s_vide,
+            o_nb_items      => o_nb_items
     );
 
 iteration_detect :  detectzero
     Port map ( 
-            i_clk => i_clk,
-            id_reset => i_reset,
-            i_x => line_in,
-            o_s => s_s, --Used for debugging purposes
-            o_z => s_z
+            i_clk       => i_clk,
+            id_reset    => i_reset,
+            i_x         => line_in,
+            o_s         => s_s, --Used for debugging purposes
+            o_z         => s_z
     );
 
 o_high <= s_z;
