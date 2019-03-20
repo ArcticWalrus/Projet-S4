afe2bc264bde91a027f818937cc743265389abed
unknown <tobiedesgreniers300@gmail.com>
2019-03-20 14:10:57 -0400

    dataFlow avec service

--- project-ui/src/app/data-display/data-display.component.ts
+++ project-ui/src/app/data-display/data-display.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label, BaseChartDirective } from 'ng2-charts'
 import * as pluginAnnotations from 'chartjs-plugin-annotation';
-//import { DataService } from '../services/data-service.service';
+import { DataService } from '../services/data-service.service';
 
 @Component({
   selector: 'app-data-display',
@@ -73,7 +73,7 @@ export class DataDisplayComponent implements OnInit {
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective;
 
-  constructor(/* private dataService: DataService */) { 
+  constructor(private dataService: DataService) { 
 
   }
 
@@ -87,15 +87,13 @@ export class DataDisplayComponent implements OnInit {
     }, 2000)
   }
 
-  public max = 15;
-  public min = 0;
   public compteur = 0;
   generateRandom(dataBuffer, labBuffer): void{
 
     //console.log("linechartdata in random : " +this.lineChartData);
     //console.log("linechartdata.data : " + this.lineChartData[0].data[0]);
     //dataBuffer[0].data.push(Math.floor(Math.random()*(this.max-this.min+1)+this.min));
-    dataBuffer[0].data.push(Math.floor(Math.random()*(this.max-this.min+1)+this.min)); //Math.floor(Math.random()*(this.max-this.min+1)+this.min);
+    dataBuffer[0].data.push(this.dataService.getData()); //Math.floor(Math.random()*(this.max-this.min+1)+this.min);
     this.compteur++;
     this.lineChartData = dataBuffer;
     this.lineChartLabels = labBuffer.slice(1,this.compteur);
--- project-ui/src/app/services/data-service.service.ts
+++ project-ui/src/app/services/data-service.service.ts
@@ -7,5 +7,9 @@ export class DataService {
 
   constructor() { }
 
-  
+  getData(){
+    let max = 15;
+    let min = 0;
+    return (Math.floor(Math.random()*(max-min+1)+min));
+  }
 }
