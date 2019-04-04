e569ba83721accde07a4893d8959431a53d75479
Malcolm St. John <stjm2505@usherbrooke.ca>
2019-04-04 12:03:09 -0400

    Test c sur board

---
+++ ergonomie/test_port_c/test_c/test_c.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_imu_c.elf_on_local.tcl
@@ -0,0 +1,16 @@
+connect -url tcp:127.0.0.1:3121
+source D:/Projet-S4/ergonomie/test_port_c/test_c/test_c.sdk/zybo_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo Z7 210351A77C2BA"} -index 0
+loadhw -hw D:/Projet-S4/ergonomie/test_port_c/test_c/test_c.sdk/zybo_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo Z7 210351A77C2BA"} -index 0
+stop
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo Z7 210351A77C2BA"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo Z7 210351A77C2BA"} -index 0
+dow D:/Projet-S4/ergonomie/test_port_c/test_c/test_c.sdk/IMU_C/Debug/IMU_C.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo Z7 210351A77C2BA"} -index 0
+con
